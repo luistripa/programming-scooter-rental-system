2df231776bf52c78d756e42a19af540c2f6cebc1
luistripa <54379665+luistripa@users.noreply.github.com>
2019-11-21 18:24:40 +0000

    Created Code files

---
+++ Code/Client.java
@@ -0,0 +1,14 @@
+public class Client {
+
+    String clientNIF;
+    String clientEmail;
+    String clientPhone;
+    String clientName;
+
+    public Client(String clientNIF, String clientEmail, String clientPhone, String clientName) {
+        this.clientNIF = clientNIF;
+        this.clientEmail = clientEmail;
+        this.clientPhone = clientPhone;
+        this.clientName = clientName;
+    }
+}
---
+++ Code/Scooter.java
@@ -0,0 +1,10 @@
+public class Scooter {
+
+    String scooterID;
+    String scooterLicensePlate;
+
+    public Scooter(String scooterID, String scooterLicencePlate) {
+        this.scooterID = scooterID;
+        this.scooterLicensePlate = scooterLicensePlate;
+    }
+}
