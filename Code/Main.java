7a8a85822c24bb4d2ff3fe237b3f4af22ba6d418
luistripa <54379665+luistripa@users.noreply.github.com>
2019-11-22 09:33:56 +0000

    Added constants

--- Code/Main.java
+++ Code/Main.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 
 public class Main {
 
+    // Constants
+	private static final String INSERT_CLIENT = "ADCLIENTE";
+	private static final String REMOVE_CLIENT = "REMCLIENTE";
+	private static final String INSERT_SCOOTER = "ADTROT";
+	private static final String CLIENT_DATA = "DADOSCLIENTE";
+	private static final String RENTED_SCOOTER = "TROT";
+	private static final String SCOOTER_DATA = "DADOSTROT";
+	private static final String RENTER = "CLIENTE";
+	private static final String ADD_BALANCE = "CARRSALDO";
+	private static final String RENT = "ALUGAR";
+	private static final String RELEASE = "LIBERTAR";
+	private static final String SYS_STATE = "ESTADOSISTEMA";
+	private static final String EXIT = "SAIR";
+	private static final String DEACTIVATE = "DESTROT";
+	private static final String ACTIVATE = "REACTROT";
+
     public static void main(String[] args) {
 
     }
