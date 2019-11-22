70b306e0d2ab42a5f10f3d8a94b12faf8477790c
luistripa <54379665+luistripa@users.noreply.github.com>
2019-11-22 19:30:07 +0000

    Added compile functionality

--- TestParser.py
+++ TestParser.py
@@ -1,8 +1,15 @@
 from os import system
 
+# Try to compile code
+if system("(cd bin ; javac ../Code/Main.java)")==0:
+	print("Files successfully compiled.")
+else:
+	print("Files failed to compile.")
+
+# Run the tests
 for i in range(1, 19):
 	i = str(i)
-	command = "java Code/Main < tests/T"+i+"/input > tests/T"+i+"/myOut ";
+	command = "java bin/Main < tests/T"+i+"/input > tests/T"+i+"/myOut ";
 	code1 = system(command)
 	command = "diff tests/T"+i+"/output tests/T"+i+"/myOut"
 	code2 = system(command)
