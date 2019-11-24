bd09523b56180ad86ac0774570b4673dc8438f98
António Duarte <antonioduarte@users.noreply.github.com>
2019-11-24 15:00:55 +0000

    Added António's first project files

---
+++ Antonio_Code/Main.java
@@ -0,0 +1,405 @@
+/**
+ * @author Antonio Duarte
+ **/
+
+import java.util.Scanner;
+
+public class Main {
+
+    // Constants
+
+    private static final String ADD_CLIENT = "ADCLIENTE";
+    private static final String ADD_BALANCE = "CARRSALDO";
+    private static final String REMOVE_CLIENT = "REMCLIENTE";
+    private static final String ADD_SCOOTER = "ADTROT";
+    private static final String CLIENT_DATA = "DADOSCLIENTE";
+    private static final String SCOOTER_DATA = "DADOSTROT";
+    private static final String RENT_SCOOTER = "ALUGAR";
+    private static final String RELEASE_SCOOTER = "LIBERTAR";
+    private static final String SYSTEM_STATE = "ESTADOSISTEMA";
+    private static final String CLIENT_WHO_RENTED = "CLIENTE";
+    private static final String SCOOTER_RENTED_BY = "TROT";
+    private static final String EXIT = "SAIR";
+    private static final String PROMOTION = "PROMOCAO";
+    private static final String DEACTIVATE_SCOOTER = "DESTROT";
+    private static final String REACTIVATE_SCOOTER = "REACTROT";
+
+    public static void main(String[] args) {
+        RentalSystem system = new RentalSystem();
+        Scanner scanner = new Scanner(System.in);
+        String option;
+        do {
+            option = readOption(scanner);
+            executeOption(scanner, system, option);
+        } while((!option.equalsIgnoreCase(EXIT)));
+        scanner.close();
+    }
+
+    /**
+     * Reads the option that the user wishes to perform.
+     * 
+     * @param scanner Reads the user's input.
+     * @return Option that the user wrote.
+     */
+    private static String readOption(Scanner scanner) {
+        return scanner.next().toUpperCase();
+    }
+
+    /**
+     * This method executes the option that the user chose to do in the program.
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     * @param option Option that the user chose to perform.
+     */
+    private static void executeOption(Scanner scanner, RentalSystem system, String option) {
+        switch (option) {
+            case ADD_CLIENT:
+                createClient(scanner, system);
+                break;
+            case REMOVE_CLIENT:
+                removeClient(scanner, system);
+                break;
+            case ADD_SCOOTER:
+                createScooter(scanner, system);
+                break;
+            case CLIENT_DATA:
+                showClientData(scanner, system);
+                break;
+            case SCOOTER_RENTED_BY:
+                scooterRentedByClient(scanner, system);
+                break;
+            case SCOOTER_DATA:
+                showScooterData(scanner, system);
+                break;
+            case CLIENT_WHO_RENTED:
+                clientRentedScooter(scanner, system);
+                break;
+            case ADD_BALANCE:
+                addBalance(scanner, system);
+                break;
+            case RENT_SCOOTER:
+                rentScooter(scanner, system);
+                break;
+            case RELEASE_SCOOTER:
+                releaseScooter(scanner, system);
+                break;
+            case SYSTEM_STATE:
+                systemState(system);
+                break;
+            case EXIT:
+                exit(system);
+                break;
+            case PROMOTION:
+                applyPromotion(scanner, system);
+                break;
+            case DEACTIVATE_SCOOTER:
+                deactivateScooter(scanner, system);
+                break;
+            case REACTIVATE_SCOOTER:
+                reactivateScooter(scanner, system);
+                break;
+            default:
+                System.out.println("Comando invalido.");
+        }
+    }
+
+    /**
+     * Prints the information about the scooter that the Client rented.
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     */
+    private static void scooterRentedByClient(Scanner scanner, RentalSystem system) {
+        String nif = scanner.next();
+        if (isNIFEqual(nif, system)) {
+            if (system.hasClientRented())
+                System.out.println(system.getIdRentedScooter() + ", " + system.getRegistrationRentedScooter());
+            else
+                System.out.println("Cliente sem trotinete.");
+        } else
+            System.out.println("Cliente inexistente.");
+    }
+
+    /**
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     */
+    private static void clientRentedScooter(Scanner scanner, RentalSystem system) {
+        String ScooterID = scanner.next();
+        if (isScooterIDEqual(ScooterID, system)) {
+            if (system.isScooterMoving())
+                System.out.println(system.getClienteWhoRentedNIF() + ", " + system.getClientWhoRentedName());
+            else
+                System.out.println("Trotinete nao alugada.");
+        } else
+            System.out.println("Trotinete inexistente.");
+    }
+
+    /**
+     * Exists the program.
+     * 
+     * @param system Rental system that is currently being used.
+     */
+    private static void exit(RentalSystem system) {
+        System.out.println("Saindo...");
+        systemState(system);
+    }
+
+    /**
+     * Prints the state of the current Rental System.
+     * 
+     * @param system
+     */
+    private static void systemState(RentalSystem system) {
+        System.out.println("Estado actual: " + system.getTotalNumberRentals() + ", " + system.getTotalMoneyPaid() + ", " + system.getTotalLateMinutes());
+    }
+
+    /**
+     * Adds a specified amount to the client's balance.
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     */
+    private static void addBalance(Scanner scanner, RentalSystem system) {
+        String nif = scanner.next();
+        int amount = scanner.nextInt();
+        scanner.nextLine();
+        if (isNIFEqual(nif, system) && amount > 0) {
+            system.addBalance(amount);
+            System.out.println("Carregamento efectuado.");
+        } else if (amount <= 0)
+            System.out.println("Valor invalido.");
+        if (!isNIFEqual(nif, system))
+            System.out.println("Cliente inexistente.");
+    }
+
+    /**
+     * Removes a client.
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     */
+    private static void removeClient(Scanner scanner, RentalSystem system) {
+        String nif = scanner.next();
+        scanner.nextLine();
+        if (isNIFEqual(nif, system)) {
+            if (!system.hasClientRented()) {
+                system.removeClient();
+                System.out.println("Cliente removido com sucesso.");
+            } else
+                System.out.println("Cliente em movimento.");
+        } else
+            System.out.println("Cliente inexistente.");
+    }
+
+    /**
+     * Rents a scooter.
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     */
+    private static void rentScooter(Scanner scanner, RentalSystem system) {
+        String nif = scanner.next();
+        String scooterID = scanner.next();
+        scanner.nextLine();
+        if (isNIFEqual(nif, system) && isScooterIDEqual(scooterID, system) && system.getBalance() >= 100 && !system.isScooterMoving() && system.isScooterActivated()) {
+            system.rentScooter();
+            System.out.println("Aluguer efectuado com sucesso.");
+        } else if (!isNIFEqual(nif, system))
+            System.out.println("Cliente inexistente.");
+        else if (!isScooterIDEqual(scooterID, system))
+            System.out.println("Trotinete inexistente.");
+        else if (system.isScooterMoving() || !system.isScooterActivated())
+            System.out.println("Trotinete nao pode ser alugada.");
+        else if (system.getBalance() < 100)
+            System.out.println("Cliente sem saldo suficiente.");
+    }
+
+    /**
+     * Releases the scooter.
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     */
+    private static void releaseScooter(Scanner scanner, RentalSystem system) {
+        String scooterID = scanner.next();
+        int minutes = scanner.nextInt();
+        scanner.nextLine();
+        if (system.isScooterMoving() && minutes > 0 && isScooterIDEqual(scooterID, system)) {
+            system.releaseScooter(minutes);
+            System.out.println("Aluguer terminado.");
+        } else if (minutes <= 0) {
+            System.out.println("Valor invalido.");
+        } else if (!isScooterIDEqual(scooterID, system)) {
+            System.out.println("Trotinete inexistente.");
+        } else if (!system.isScooterMoving()) {
+            System.out.println("Trotinete nao alugada.");
+        }
+    }
+
+    /**
+     * Creates a client.
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     */
+    private static void createClient(Scanner scanner, RentalSystem system) {
+        String nif = scanner.next();
+        String email = scanner.next();
+        int phone = scanner.nextInt();
+        String name = scanner.nextLine().trim();
+        if (isNIFEqual(nif, system)) {
+            System.out.println("Cliente existente.");
+        } else {
+            system.createClient(nif, email, phone, name);
+            System.out.println("Insercao de cliente com sucesso.");
+        }
+    }
+
+    /**
+     * Creates a scooter.
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     */
+    private static void createScooter(Scanner scanner, RentalSystem system) {
+        String scooterID = scanner.next();
+        String scooterRegistration = scanner.next();
+        scanner.nextLine();
+        if (isScooterIDEqual(scooterID, system)) {
+            System.out.println("Trotinete existente.");
+        } else {
+            system.createScooter(scooterID, scooterRegistration);
+            System.out.println("Insercao de trotinete com sucesso.");
+        }
+    }
+
+    /**
+     * Shows the data regarding the client that matches the provided nif.
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     */
+    private static void showClientData(Scanner scanner, RentalSystem system) {
+        String nif = scanner.next();
+        scanner.nextLine();
+        if (isNIFEqual(nif, system)) {
+            System.out.println(system.getName() + ": " + system.getNIF() + ", "
+                    + system.getEmail() + ", " + system.getPhoneNumber() + ", "
+                    + system.getBalance() + ", " + system.getMinutes() + ", "
+                    + system.getNumberRentals() + ", " + system.getMax() + ", "
+                    + system.getAverageMinutes() + ", " + system.getMoneySpent());
+        } else {
+            System.out.println("Cliente inexistente.");
+        }
+    }
+
+    /**
+     * Shows the data regarding the scooter that matches the provided scooterID.
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     */
+    private static void showScooterData(Scanner scanner, RentalSystem system) {
+        String scooterID = scanner.next();
+        scanner.nextLine();
+        if (isScooterIDEqual(scooterID, system)) {
+            String state;
+            if (system.isScooterMoving())
+                state = "alugada";
+            else if (system.isScooterActivated())
+                state = "parada";
+            else
+                state = "inactiva";
+            System.out.println(system.getScooterRegistration() + ": " + state + ", " + system.getUsageAmount()
+                    + ", " + system.getUsageMinutes());
+        } else
+            System.out.println("Trotinete inexistente.");
+    }
+
+    /**
+     * Applies a promotion to the last rental performed.
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     */
+    private static void applyPromotion(Scanner scanner, RentalSystem system) {
+        String nif = scanner.next();
+        scanner.nextLine();
+        if (isNIFEqual(nif, system) && !system.hasClientRented() && !system.isPromotionApplied()) {
+            system.promotion();
+            System.out.println("Promocao aplicada.");
+        } else if (!isNIFEqual(nif, system))
+            System.out.println("Cliente inexistente.");
+        else if (system.hasClientRented())
+            System.out.println("Cliente iniciou novo aluguer.");
+        else if (system.isPromotionApplied())
+            System.out.println("Promocao ja aplicada.");
+    }
+
+    /**
+     * Deactivates the scooter that matches the provided scooterID.
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     */
+    private static void deactivateScooter(Scanner scanner, RentalSystem system) {
+        String scooterID = scanner.next();
+        scanner.nextLine();
+        if (isScooterIDEqual(scooterID, system) && !system.isScooterMoving()) {
+            system.deactivateScooter();
+            System.out.println("Trotinete desactivada.");
+        } else if (!isScooterIDEqual(scooterID, system))
+            System.out.println("Trotinete inexistente.");
+        else if (system.isScooterMoving())
+            System.out.println("Trotinete em movimento.");
+    }
+
+    /**
+     * Reactivates the scooter that matches the provided scooterID.
+     * 
+     * @param scanner Reads the user's input.
+     * @param system Rental system that is currently being used.
+     */
+    private static void reactivateScooter(Scanner scanner, RentalSystem system) {
+        String scooterID = scanner.next();
+        scanner.nextLine();
+        if (isScooterIDEqual(scooterID, system) && !system.isScooterActivated()) {
+            system.reactivateScooter();
+            System.out.println("Trotinete reactivada.");
+        } else if (!isScooterIDEqual(scooterID, system))
+            System.out.println("Trotinete inexistente.");
+        else if (system.isScooterActivated())
+            System.out.println("Trotinete nao inactiva.");
+    }
+
+    /**
+     * Verifies if there is a client with the provided nif.
+     * 
+     * @param nif NIF that is being compared.
+     * @param system Rental system that is currently being used.
+     * @return If the NIF is or isn't equal to the one introduced.
+     */
+    private static boolean isNIFEqual(String nif, RentalSystem system) {
+        if (system.doesClientExist())
+            return nif.equalsIgnoreCase(system.getNIF());
+        else
+            return false;
+    }
+
+    /**
+     * Verifies if there is a scooter with the provided scooterID.
+     * 
+     * @param scooterID ScooterID that is being compared.
+     * @param system Rental system that is currently being used.
+     * @return If the ScooterID is or isn't equal to the one introduced.
+     */
+    private static boolean isScooterIDEqual(String scooterID, RentalSystem system) {
+        if (system.doesScooterExist())
+            return scooterID.equalsIgnoreCase(system.getScooterID());
+        else
+            return false;
+    }
+}
---
+++ Antonio_Code/RentalSystem.java
@@ -0,0 +1,404 @@
+/**
+ * @author Antonio Duarte
+ * Handles operations with the Scooter and Client classes, managing a rental system in between the two of them.
+ **/
+
+public class RentalSystem {
+
+    // Constants.
+
+    private final int MAX_MINUTES = 60;
+    private final int COST = 100;
+    private final int MINUTES_PER_FINE = 30;
+
+    // Variables.
+
+    public Scooter scooter;
+    public Client client;
+    private int totalNumberRentals, totalMoneyPaid, totalLateMinutes, lastRentalMinutes, lastRentalCost, lastRentalMax, lastLateMinutes;
+    private boolean clientExists, scooterExists, promotionApplied;
+
+    // Constructor.
+
+    public RentalSystem() {
+        totalNumberRentals = 0;
+        totalMoneyPaid = 0;
+        clientExists = false;
+        scooterExists = false;
+        lastRentalMinutes = 0;
+        lastRentalCost = 0;
+        lastRentalMax = 0;
+        lastLateMinutes = 0;
+        promotionApplied = true;
+    }
+
+    // Methods.
+
+    /**
+     *
+     * @return  NIF of the client
+     */
+    public String getNIF() {
+        return client.getNIF();
+    }
+
+    /**
+     *
+     * @return Name of the client
+     */
+    public String getName() {
+        return client.getName();
+    }
+
+    /**
+     *
+     * @return Email of the client
+     */
+    public String getEmail() {
+        return client.getEmail();
+    }
+
+    /**
+     *
+     * @return ID of the scooter that the client rented
+     */
+    public String getIdRentedScooter() {
+        return client.getIdRentedScooter();
+    }
+
+    /**
+     *
+     * @return Registration of the scooter that the client rented
+     */
+    public String getRegistrationRentedScooter() {
+        return client.getRegistrationRentedScooter();
+    }
+
+    /**
+     *
+     * @return Balance of the client
+     */
+    public int getBalance() {
+        return client.getBalance();
+    }
+
+    /**
+     *
+     * @return Number of times that the client performed a rental
+     */
+    public int getNumberRentals() {
+        return client.getNumRentals();
+    }
+
+    /**
+     *
+     * @return Average rental minutes per rental of the client
+     */
+    public int getAverageMinutes() {
+        return client.getAverageMinutes();
+    }
+
+    /**
+     *
+     * @return Maximum minutes of a single rental by the client
+     */
+    public int getMax() {
+        return client.getMax();
+    }
+
+    /**
+     *
+     * @return Total money spent by the client
+     */
+    public int getMoneySpent() {
+        return client.getMoneySpent();
+    }
+
+    /**
+     *
+     * @return Client's phone number
+     */
+    public int getPhoneNumber() {
+        return client.getPhoneNumber();
+    }
+
+    /**
+     *
+     * @return Total number of rental minutes of the client
+     */
+    public int getMinutes() {
+        return client.getMinutes();
+    }
+
+    /**
+     *
+     * @return NIF of the client who rented the scooter
+     */
+    public String getClienteWhoRentedNIF() {
+        return scooter.getClientWhoRentedNIF();
+    }
+
+    /**
+     *
+     * @return Name of the client who rented the scooter
+     */
+    public String getClientWhoRentedName() {
+        return scooter.getClientWhoRentedName();
+    }
+
+    /**
+     *
+     * @return Registration of the scooter
+     */
+    public String getScooterRegistration() {
+        return scooter.getScooterRegistration();
+    }
+
+    /**
+     *
+     * @return ID of the scooter
+     */
+    public String getScooterID() {
+        return scooter.getScooterID();
+    }
+
+    /**
+     *
+     * @return The amount of times that the scooter was rented
+     */
+    public int getUsageAmount() {
+        return scooter.getUsageAmount();
+    }
+
+    /**
+     *
+     * @return Total number of minutes that the scooter was rented
+     */
+    public int getUsageMinutes() {
+        return scooter.getUsageMinutes();
+    }
+
+    /**
+     *
+     * @return Total number of rentals performed by the system
+     */
+    public int getTotalNumberRentals() {
+        return totalNumberRentals;
+    }
+
+    /**
+     *
+     * @return Total money paid to the system
+     */
+    public int getTotalMoneyPaid() {
+        return totalMoneyPaid;
+    }
+
+    /**
+     *
+     * @return Total late minutes in rentals performed by the system
+     */
+    public int getTotalLateMinutes() {
+        return totalLateMinutes;
+    }
+
+    /**
+     *
+     * @return Returns true if there is a client in the system
+     */
+    public boolean doesClientExist() {
+        return clientExists;
+    }
+
+    /**
+     *
+     * @return Returns true if there is a scooter in the system
+     */
+    public boolean doesScooterExist() {
+        return scooterExists;
+    }
+
+    /**
+     *
+     * @param nif Identification of the client
+     * @param email Email of the client
+     * @param phoneNumber Phone number of the client
+     * @param name Name of the client
+     */
+    public void createClient(String nif, String email, int phoneNumber, String name) {
+        clientExists = true;
+        client = new Client(nif, email, phoneNumber, name);
+    }
+
+    /**
+     *
+     * @param scooterID Identification of the scooter
+     * @param scooterRegistration Registration of the scooter
+     */
+    public void createScooter(String scooterID, String scooterRegistration) {
+        scooterExists = true;
+        scooter = new Scooter(scooterID, scooterRegistration);
+    }
+
+    /**
+     * Rents the scooter by switching the isMoving variable of the scooter to true.
+     *
+     * Changes the variables clientWhoRentedNIF and clientWhoRentedName to the NIF and name of the client of the client.
+     *
+     * Changes the variables IDRentedScooter and registrationRentedScooter to the scooterID and scooterRegistration
+     * of the scooter.
+     *
+     * Resets the variables that store the values of the last rental (lastRentalCost, lastRentalMax, lastRentalMinutes
+     * and lastLateMinutes).
+     */
+    public void rentScooter() {
+        scooter.move();
+        scooter.rentedBy(client.getNIF(), client.getName());
+        client.rentScooter(scooter.getScooterID(), scooter.getScooterRegistration());
+
+        lastRentalCost = 0;
+        lastRentalMax = 0;
+        lastRentalMinutes = 0;
+        lastLateMinutes = 0;
+    }
+
+    /**
+     *
+     * @return Returns true if the client has rented a scooter. Returns false if the client hasn't rented a scooter or
+     * if there isn't a client in the system.
+     */
+    public boolean hasClientRented() {
+        if (clientExists) {
+            return client.hasRented();
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @return Returns true if the scooter has been rented. Returns false if the scooter hasn't been rented or if there
+     * isn't a scooter in the system.
+     */
+    public boolean isScooterMoving() {
+        if (scooterExists)
+            return scooter.isMoving();
+        else
+            return false;
+    }
+
+    /**
+     * Releases the scooter.
+     *
+     * Verifies if the amount of minutes that the scooter was rented for is above the maximum amount of minutes per rental.
+     * If the amount of minutes is above the maximum amount of minutes it calculates the fine and applies it to the
+     * cost of the rental.
+     *
+     * Updates the client's max rental time variable if the amount of minutes of the rental was above the previous
+     * max.
+     *
+     * Increases the total money paid to the system.
+     *
+     * Increments the total number of rentals performed by the system.
+     *
+     * Updates the variables that store the values of the last rental (lastRentalCost, lastRentalMax, lastRentalMinutes
+     * and lastLateMinutes)
+     *
+     * @param minutes an int that specifies the amount of minutes that the rental lasted for.
+     */
+    public void releaseScooter(int minutes) {
+        int lateMinutes = 0;
+        if (minutes > MAX_MINUTES) {
+            lateMinutes = minutes - MAX_MINUTES;
+            if (lateMinutes % 30 == 0) {
+                client.removeBalance(lateMinutes / MINUTES_PER_FINE * COST);
+                totalMoneyPaid += lateMinutes / MINUTES_PER_FINE * COST;
+                lastRentalCost += lateMinutes / MINUTES_PER_FINE * COST;
+            } else {
+                client.removeBalance((lateMinutes / MINUTES_PER_FINE + 1) * COST);
+                totalMoneyPaid += (lateMinutes / MINUTES_PER_FINE + 1) * COST;
+                lastRentalCost += (lateMinutes / MINUTES_PER_FINE + 1) * COST;
+            }
+            totalLateMinutes += lateMinutes;
+        }
+        client.releaseScooter(minutes);
+        client.removeBalance(COST);
+        scooter.release(minutes);
+        totalMoneyPaid += COST;
+        totalNumberRentals++;
+        lastRentalMinutes = minutes;
+        lastLateMinutes = lateMinutes;
+        promotionApplied = false;
+        lastRentalCost += COST;
+    }
+
+    /**
+     *
+     * @param amount
+     */
+    public void addBalance(int amount) {
+        client.addBalance(amount);
+    }
+
+    /**
+     * Removes the client by setting the client variable to null and the clientExists boolean to false.
+     */
+    public void removeClient() {
+        client = null;
+        clientExists = false;
+    }
+
+    /**
+     * 
+     * @return If a promotion is or isn't applied.
+     */
+    public boolean isPromotionApplied() {
+        return promotionApplied;
+    }
+
+    /**
+     * Deactivates the scooter.
+     */
+    public void deactivateScooter() {
+        scooter.deactivate();
+    }
+
+    /**
+     * Reactivates the scooter.
+     */
+    public void reactivateScooter() {
+        scooter.activate();
+    }
+
+    /**
+     *
+     * @return True if a scooter exists and it is activated. Returns false if it's deactivated.
+     */
+    public boolean isScooterActivated() {
+        if (scooterExists) {
+            return scooter.isActivated();
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Method that applies a promotion to the last rental performed by the client
+     * by reverting the Client, Scooter and System variables to the state that they were at
+     * previous to the last rental.
+     */
+    public void promotion() {
+        if (!client.hasRented()) {
+            client.addBalance(lastRentalCost);
+            client.addMinutes(-lastRentalMinutes);
+            client.setMax(lastRentalMax);
+            client.decrementNumberRentals();
+            client.addMoneySpent(-lastRentalCost);
+            scooter.decrementUsageAmount();
+            scooter.setTotalMinutes(-lastRentalMinutes);
+            totalNumberRentals--;
+            totalMoneyPaid -= lastRentalCost;
+            totalLateMinutes -= lastLateMinutes;
+            promotionApplied = true;
+        }
+    }
+}
