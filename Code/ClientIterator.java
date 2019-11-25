78299b7f6af24a7f518b870e0d05f0e756f06865
António Duarte <antonioduarte@users.noreply.github.com>
2019-11-25 09:08:44 +0000

    Update ClientIterator.java

--- Code/ClientIterator.java
+++ Code/ClientIterator.java
@@ -3,6 +3,18 @@ public class ClientIterator {
     private int counter;
     private int nextClient;
     private Client[] clients;
-
     
+    public ClientIterator(int counter, Client[] clients) {
+        this.counter = counter;
+        this.clients = clients;
+        nextClient = 0;
+    }
+
+    public boolean hasNext() {
+        return nextClient < counter;
+    }
+
+    public Client next() {
+        return clients[nextClient++];
+    }
 }
