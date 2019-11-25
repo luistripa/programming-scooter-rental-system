0e9662c20c1722c7d512338ad5b6c6104f3a673e
luistripa <54379665+luistripa@users.noreply.github.com>
2019-11-25 09:38:28 +0000

    Updated RentalSystem

--- Code/RentalSystem.java
+++ Code/RentalSystem.java
@@ -13,8 +13,8 @@ public class RentalSystem {
     private final int MINUTES_PER_FINE = 30;
 
     // Instance Variables
-    private Client[] client;
-    private Scooter[] scooter;
+    private Client[] clients;
+    private Scooter[] scooters;
     private int scooterCounter;
     private int clientCounter;
     private int totalRentals;
@@ -38,26 +38,159 @@ public class RentalSystem {
      * @param email The client email
      * @param phone The client phone
      * @param name The client name
-     * PRE: !clientExists(nif)
+     * PRE: searchIndexOfClient(nif)==-1
      */
     public void insertClient(String nif, String email, int phone, String name) {
-        client[clientCounter] = new Client(nif, email, phone, name);
+        clients[clientCounter] = new Client(nif, email, phone, name);
         clientCounter++;
     }
 
     /**
-     * Tries to find the given nif in the Client vector
+     * Tries to find the given nif in the Client vector. Returns -1 if not found
      * @param nif The nif to be searched
-     * @return A boolean representing if the nif was or not found
+     * @return The position of the client in the vector
      */
-    public boolean clientExists(String nif) {
-        for (int i=0 ; i<clientCounter ; i++) {
-            if (Client[i].getNif.equals(nif))
-                return true;
+    public int searchIndexOfClient(String nif) {
+        int pos=-1;
+        for (int i=0 ; i<clientCounter&&pos==-1 ; i++) {
+            if (clients[i].getNif.equalsIgnoreCase(nif))
+                pos=i;
+        }
+        return pos;
+    }
+
+    /**
+     * Removes the client with given nif
+     * @param nif The client nif
+     * PRE: searchIndexOfClient(nif)!=-1
+     */
+    public void removeClient(String nif) {
+        int pos = searchIndexOfClient(nif);
+        for (int i=pos ; i<clientCounter-1 ; i++) {
+            clients[i] = clients[i+1];
         }
     }
 
-    public void insertScooter()
+    /**
+     * Gets the client nif as it is stored in the system
+     * @param nif The client nif
+     * @return The client nif as it is stored in the system
+     * PRE: searchIndexOfClient(nif)!=-1
+     */
+    public String getClientNif(String nif) {
+        return clients[searchIndexOfClient].getNif();
+    }
+
+    /**
+     * Gets the client email stored in the system
+     * @param nif The client nif
+     * @return The client email
+     * PRE: searchIndexOfClient(nif)!=-1
+     */
+    public String getClientEmail(String nif) {
+        return clients[searchIndexOfClient(nif)].getEmail();
+    }
+
+    /**
+     * Gets the client phone stored in the system
+     * @param  nif The client nif
+     * @return The client phone
+     * PRE: searchIndexOfClient(nif)!=-1
+     */
+    public String getClientPhone(String nif) {
+        return clients[searchIndexOfClient(nif)].getPhone();
+    }
+
+    /**
+     * Gets the client name stored in the system
+     * @param  nif The client nif
+     * @return The client name
+     * PRE: searchIndexOfClient(nif)!=-1
+     */
+    public String getClientName(String nif) {
+        return clients[searchIndexOfClient(nif)].getName();
+    }
+
+    /**
+     * Gets the client balance stored in the system
+     * @param  nif Thge client nif
+     * @return The client balance
+     * PRE: searchIndexOfClient(nif)!=-1
+     */
+    public int getClientBalance(int nif) {
+        return clients[searchIndexOfClient(nif)].getBalance();
+    }
+
+    /**
+     * Gets the client total moving minutes
+     * @param  nif The client nif
+     * @return The client total moving minutes
+     * PRE: searchIndexOfClient(nif)!=-1
+     */
+    public int getClientTotalMinutes(String nif) {
+        return clients[searchIndexOfClient(nif)].getTotalMinutes();
+    }
+
+    /**
+     * Gets the client total number of rentals
+     * @param  nif The client nif
+     * @return The client total number of rentals
+     * PRE: searchIndexOfClient(nif)!=-1
+     */
+    public int getClientNumberRentals(String nif) {
+        return clients[searchIndexOfClient(nif)].getNumberRentals();
+    }
+
+    /**
+     * Gets the client max time in a rental
+     * @param  nif The client nif
+     * @return The client max time in a rental
+     * PRE: searchIndexOfClient(nif)!=-1
+     */
+    public int getClientMaxTime(String nif) {
+        return clients[searchIndexOfClient(nif)].getMaxTime();
+    }
+
+    /**
+     * Gets the client average rental minutes
+     * @param  nif The client nif
+     * @return The client average rental minutes
+     * PRE: searchIndexOfClient(nif)!=-1
+     */
+    public int getClientAverageRentalTime(String nif) {
+        return clients[searchIndexOfClient(nif)].getAverageMinutes();
+    }
+
+    /**
+     * Gets the client total money spent
+     * @param  nif The client nif
+     * @return The client total money spent
+     * PRE: searchIndexOfClient(nif)!=-1
+     */
+    public int getClientMoneySpent(String nif) {
+        return clients[searchIndexOfClient(nif)].getMoneySpent();
+    }
+
+    /**
+     * Inserts a scooter in the system
+     * @param id           [description]
+     * @param registration [description]
+     * PRE: searchIndexOfScooter(id)==-1
+     */
+    public void insertScooter(String id, String registration) {
+        scooters[scooterCounter] = new Scooter(id, registration);
+        scooterCounter++;
+    }
+
+    public int searchIndexOfScooter(String id) {
+        int pos=-1;
+        for (int i=0 ; i<scooterCounter&&pos==-1 ; i++) {
+            if (scooters[i].getScooterID().equalsIgnoreCase(id)) {
+                pos = i;
+            }
+        }
+        return pos;
+    }
 
 
 
